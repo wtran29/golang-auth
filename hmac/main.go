7b2693efa2e4359305bd539d7450a24d8bb90891
William Tran <wtran29@gmail.com>
2023-05-17 16:22:28 -0700

    hmac cookie example complete

--- hmac/main.go
+++ hmac/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 )
 
 func main() {
@@ -48,6 +49,20 @@ func foo(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		c = &http.Cookie{}
 	}
+
+	isEqual := true
+	xs := strings.SplitN(c.Value, "|", 2)
+	if len(xs) == 2 {
+		cCode := xs[0]
+		cEmail := xs[1]
+
+		code := getCode(cEmail)
+		isEqual = hmac.Equal([]byte(cCode), []byte(code))
+	}
+	message := "Not logged in"
+	if isEqual {
+		message = "Logged in"
+	}
 	html := `<!DOCTYPE html>
 	<html lang="en">
 	<head>
@@ -57,7 +72,8 @@ func foo(w http.ResponseWriter, r *http.Request) {
 		<title>HMAC Example</title>
 	</head>
 	<body>
-	<p>Cookie value: ` + c.Value + `</p>
+		<p>Cookie value: ` + c.Value + `</p>
+		<p>` + message + `</p>
 		<form action="/submit" method="post">
 			<input type="email" name="email" />
 			<input type="submit" />
